29f2c88cde0b2fff866d8ef71d22641eddac862a
vagrant <vagrant@homestead>
2018-07-13 01:24:43 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,21 +9,24 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 获取用户头像
+     * @param string $size
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        $result = 'http://www.gravatar.com/avatar/'.$hash.'?s='.$size;
+
+        return $result;
+    }
 }

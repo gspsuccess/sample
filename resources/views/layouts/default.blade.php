1bcc3dbb425d08cca738eceef74a3248a808bfd1
vagrant <vagrant@homestead>
2018-07-13 01:55:04 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,10 +8,11 @@
   @include('layouts._header')
   <div class="container">
     <div class="col-md-offset-1 col-md-10">
-      @include('shared.messages')
+      @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
     </div>
   </div>
+  <script src="/js/app.js"></script>
   </body>
 </html>
